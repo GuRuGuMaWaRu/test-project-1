16d3b84664bd24055b9087f3da84563bd63dd5b2
GuRuGu <peter.k.nets@gmail.com>
2021-07-17 08:49:00 +0300

    Remove the old folder

--- 1-parse-int/newParseInt.js
+++
@@ -1,8 +0,0 @@
-const newParseInt = textNum => Number(textNum)
-
-// Tests
-console.log(newParseInt('42'));
-console.log(newParseInt('42') + 2);
-console.log(newParseInt('421') - 12);
-console.log(newParseInt('12') * 1);
-console.log(newParseInt('100') / 10);
